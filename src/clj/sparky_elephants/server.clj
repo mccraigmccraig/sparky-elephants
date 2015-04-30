b251f95e9052564ce8bbb83eb1882f0802b4f620
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-04-30 13:03:53 +0100

    better server control

--- src/clj/sparky_elephants/server.clj
+++ src/clj/sparky_elephants/server.clj
@@ -1,19 +1,25 @@
 (ns sparky-elephants.server
-  (:require [sparky-elephants.handler :refer [app]]
+  (:require sparky-elephants.handler
             [aleph.http :as http])
   (:gen-class))
 
 (defn create-server
-  [port]
-  (http/start-server app {:port port}))
+  [port & [{:keys [handler] :or {handler sparky-elephants.handler/app}}]]
+  (http/start-server handler {:port port}))
 
 (defonce server-atom (atom nil))
 
 (defn start-or-restart-server
-  [port]
+  [port & [opts]]
   (swap! server-atom (fn [old-server]
                        (when old-server (.close old-server))
-                       (create-server port))))
+                       (create-server port opts))))
+
+(defn stop-server
+  []
+  (swap! server-atom (fn [old-server]
+                       (when old-server (.close old-server))
+                       nil)))
 
  (defn -main [& args]
    (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
